2274df3ca9c0b527ab2411bfd4f8fb43e46e2338
SunilBaghel002 <sunilbaghel93100@gmail.com>
2025-05-02 22:07:02 +0530

    update home.html
    html structure of home page is added and log-in page added

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const app = express();
-const path = require("path")
+const path = require("path");
 const port = 5000;
 
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "/views/index.html"))
+  res.sendFile(path.join(__dirname, "/views/home.html"))
+);
+app.get("/login", (req, res) =>
+  res.sendFile(path.join(__dirname, "/views/log-in.html"))
 );
 
 app.listen(port, () => {
-    console.log(`Server starts at port ${port}`);
+  console.log(`Server starts at port ${port}`);
 });
